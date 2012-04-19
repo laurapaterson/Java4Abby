6cf2dcff6ddefa759b9119d9deff8abb80b5218b
Laura Paterson <laura.paterson@gmail.com>
2012-04-19 14:26:11 +0530

    added failing test

---
+++ src/test/java/com/thoughtworks/twu/java4Abby/FailingTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.twu.java4Abby;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class FailingTest {
+
+    @Test
+    public void this_test_should_fail() {
+        fail();
+    }
+
+
+}
